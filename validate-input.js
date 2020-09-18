314c8625fd8026cb75e146375b8803e54fad4a58
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-09-18 13:59:59 +0100

    Fix input validation

--- validate-input.js
+++ validate-input.js
@@ -1,5 +1,11 @@
+const allowed = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`];
+
 module.exports = function validateInput(input) {
-  const isString = typeof input === 'string';
-  const isStringOfDigits = isString && input.match(/[0-9]+/);
-  return isStringOfDigits;
+  const isString = typeof input === `string`;
+
+  const characters = input.split(``);
+  const invalidCharacters = characters.filter((char) => !allowed.includes(char));
+
+  const isStringOfAllowedChars = isString && invalidCharacters.length === 0;
+  return isStringOfAllowedChars;
 }
