2c548adfda0516259c30b8917e83ce8f57ad2262
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-09-18 18:22:23 +0100

    More consistent quotes

--- left-pad.test.js
+++ left-pad.test.js
@@ -4,9 +4,9 @@ const leftPad = require(`./left-pad`);
 module.exports = function run () {
   console.log(`Running tests from ${__filename}`)
 
-  assert.strictEqual(leftPad('1', 2), '01');
-  assert.strictEqual(leftPad('1', 3), '001');
-  assert.strictEqual(leftPad('1', 1), '1');
-  assert.strictEqual(leftPad('1', 0), '1');
-  assert.strictEqual(leftPad('1', -1), '1');
+  assert.strictEqual(leftPad(`1`, 2), `01`);
+  assert.strictEqual(leftPad(`1`, 3), `001`);
+  assert.strictEqual(leftPad(`1`, 1), `1`);
+  assert.strictEqual(leftPad(`1`, 0), `1`);
+  assert.strictEqual(leftPad(`1`, -1), `1`);
 }
