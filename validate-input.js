4f7b1893a6eed3b37925e085ca7494ba9d0dff3c
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-09-18 14:00:55 +0100

    Add TODO

--- validate-input.js
+++ validate-input.js
@@ -1,3 +1,4 @@
+// TODO: Fix magic coupling between this and keys of characters
 const allowed = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, ':'];
 
 module.exports = function validateInput(input) {
