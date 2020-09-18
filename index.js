5454f09827e98e208cbd7b861cd3e883ce146b1d
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-09-18 11:51:52 +0100

    Add a little breathing room

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const convert = require(`./convert`);
+
 const input = process.argv[2];
+
 // TODO: Check valid input, i.e. string of digits 0-9
+
 const output = convert(input);
+
 console.log(output);
