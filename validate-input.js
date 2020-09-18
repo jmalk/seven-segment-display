f11f895e5d46dbb4af7e456f0521a4d0ae38a685
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-09-18 14:00:37 +0100

    Add colon character

--- validate-input.js
+++ validate-input.js
@@ -1,4 +1,4 @@
-const allowed = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`];
+const allowed = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, ':'];
 
 module.exports = function validateInput(input) {
   const isString = typeof input === `string`;
