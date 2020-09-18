91b28e7e704487392921fe5e4a991090019c3056
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-09-18 18:23:41 +0100

    More consistent quotes

--- left-pad.js
+++ left-pad.js
@@ -1,4 +1,4 @@
-module.exports = function leftPad(string, minimumLength, char = '0') {
+module.exports = function leftPad(string, minimumLength, char = `0`) {
   return string.length < minimumLength
     ? leftPad(`${char}${string}`, minimumLength, char)
     : string;
