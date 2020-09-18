2002b2d52056d3fa97b99b606097398a79eabba6
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-09-18 14:55:13 +0100

    Fix left-pad usage bug

--- time-to-string.js
+++ time-to-string.js
@@ -2,6 +2,7 @@ const leftPad = require(`./left-pad`);
 
 module.exports = function ({hours, minutes, seconds}) {
   return [hours, minutes, seconds]
+    .map(String)
     .map((str) => leftPad(str, 2))
     .join(':');
 }
