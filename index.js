5c623abcf293fb62ed4086f102e5be4eda715f21
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-09-18 17:33:57 +0100

    Rename output -> ticker

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const currentTime = require(`./current-time`);
 const timeToString = require(`./time-to-string`);
 const validateInput = require(`./validate-input`);
 const convert = require(`./convert`);
-const output = require(`./output`);
+const ticker = require(`./ticker`);
 
 function update() {
   const string = timeToString(currentTime());
@@ -10,7 +10,7 @@ function update() {
 
   if (isValid) {
     const result = convert(string);
-    output(result);
+    ticker(result);
   } else {
     console.error(`Generated an invalid string from the current time. Please file a bug report at https://github.com/jmalk/seven-segment-display/issues`);
     process.exit(1);
